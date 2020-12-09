83f679ff57d9830edddb7f21c6ab8dd640bbb51c
wpdough <me@wpdough.com>
2020-12-09 18:17:29 -0500

    add some problem api

---
+++ problem-api/src/main/java/com/wpdough/aoc/AocProblem.java
@@ -0,0 +1,24 @@
+package com.wpdough.aoc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AocProblem<T> {
+
+    public abstract T parseInput(List<String> input);
+
+    public abstract long partOne(T input);
+
+    public abstract long partTwo(T input);
+
+    public void solve(String fileName) throws IOException {
+        List<String> fileLines = Files.lines(Paths.get(fileName))
+                .collect(Collectors.toList());
+        T input = parseInput(fileLines);
+        System.out.println(partOne(input));
+        System.out.println(partTwo(input));
+    }
+}
